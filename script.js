bceb58658572c23f824c790a4e245b09ec990dc1
Druv <druvkotwani12@gmail.com>
2023-03-24 15:21:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let score = 20;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+const secretDisplay = function (secretNumber) {
+  document.querySelector('.number').textContent = secretNumber;
+};
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  displayScore(score);
+  secretDisplay('?');
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('❌ No number!');
+  }
+  // When the player wins
+  else if (secretNumber == guess) {
+    displayMessage('🎉 Correct Number!');
+    secretDisplay(secretNumber);
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  }
+  // When the guess is different
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      displayScore(score);
+    } else {
+      displayScore(0);
+      displayMessage('🎆 You lost the game!');
+    }
+  }
+});
